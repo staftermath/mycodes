793f33b711abe9bf0eea78359196a885e1108920
staftermath <gwengww@gmail.com>
2016-03-26 22:06:50 -0500

    ProgressBar that works under console

--- ProgressBar.py
+++ ProgressBar.py
@@ -8,9 +8,17 @@ class ProgressBar:
         self.counter = 0
 
     def update(self):
+        if self.counter > self.total:
+            self.reset()
         self.counter += 1
         percent = float(self.counter) / self.total
         hashes = self.symbol * int(round(percent * self.barlength))
         spaces = ' ' * (self.barlength - len(hashes))
         sys.stdout.write("\rPercent: [{0}] {1}%".format(hashes + spaces, int(round(percent * 100))))
         sys.stdout.flush()
+        if self.counter == self.total:
+            sys.stdout.write("\n")
+            self.reset()
+
+    def reset(self):
+        self.counter = 0
