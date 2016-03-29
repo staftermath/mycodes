38df89c8eb8a6a43cf565b90418c6ad10176a0f4
staftermath <gwengww@gmail.com>
2016-03-28 20:33:33 -0500

    Added files via upload
    Bug Fixed.

--- ProgressBar.py
+++ ProgressBar.py
@@ -1,24 +1,29 @@
 import sys
 
+
 class ProgressBar:
     def __init__(self,total,barlength=50,symbol = "#"):
         self.total = total
         self.barlength = barlength
         self.symbol = symbol
         self.counter = 0
+        self.lastcounter = 0
 
     def update(self):
         if self.counter > self.total:
             self.reset()
         self.counter += 1
-        percent = float(self.counter) / self.total
-        hashes = self.symbol * int(round(percent * self.barlength))
-        spaces = ' ' * (self.barlength - len(hashes))
-        sys.stdout.write("\rPercent: [{0}] {1}%".format(hashes + spaces, int(round(percent * 100))))
-        sys.stdout.flush()
+        if float(self.counter - self.lastcounter)/self.total >= 0.01:
+            percent = float(self.counter) / self.total
+            hashes = self.symbol * int(round(percent * self.barlength))
+            spaces = ' ' * (self.barlength - len(hashes))
+            sys.stdout.write("\rPercent: [{0}] {1}%".format(hashes + spaces, int(round(percent * 100))))
+            sys.stdout.flush()
+            self.lastcounter = self.counter
         if self.counter == self.total:
             sys.stdout.write("\n")
             self.reset()
 
     def reset(self):
         self.counter = 0
+
